97ce87378503ae15563580c6f68d757113a67331
CJM <whdals9514@naver.com>
2023-04-29 15:43:22 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace TelnetClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.rtbResponse = new System.Windows.Forms.RichTextBox();
+            this.xlsxBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(68, 49);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(186, 45);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // rtbResponse
+            // 
+            this.rtbResponse.Location = new System.Drawing.Point(68, 110);
+            this.rtbResponse.Name = "rtbResponse";
+            this.rtbResponse.Size = new System.Drawing.Size(293, 96);
+            this.rtbResponse.TabIndex = 4;
+            this.rtbResponse.Text = "";
+            // 
+            // xlsxBtn
+            // 
+            this.xlsxBtn.Location = new System.Drawing.Point(463, 49);
+            this.xlsxBtn.Name = "xlsxBtn";
+            this.xlsxBtn.Size = new System.Drawing.Size(217, 45);
+            this.xlsxBtn.TabIndex = 5;
+            this.xlsxBtn.Text = "Exel save";
+            this.xlsxBtn.UseVisualStyleBackColor = true;
+            this.xlsxBtn.Click += new System.EventHandler(this.xlsxBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.xlsxBtn);
+            this.Controls.Add(this.rtbResponse);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox rtbResponse;
+        private System.Windows.Forms.Button xlsxBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TelnetClient
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application excel = new Excel.Application();
+        Excel.Workbook workbook;
+        Excel.Worksheet worksheet;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Connect to the telnet server
+            TcpClient client = new TcpClient("172.30.1.99", 1234);
+            NetworkStream stream = client.GetStream();
+
+            // Send the command
+            byte[] command = Encoding.ASCII.GetBytes("command\r\n");
+            stream.Write(command, 0, command.Length);
+
+            // Read the response
+            byte[] response = new byte[1024];
+            int bytesRead = stream.Read(response, 0, response.Length);
+            string responseData = Encoding.ASCII.GetString(response, 0, bytesRead);
+
+            rtbResponse.AppendText(responseData);
+            // Display the response
+            MessageBox.Show(responseData);
+
+            // Close the connection
+            stream.Close();
+            client.Close();
+        }
+
+        private void xlsxBtn_Click(object sender, EventArgs e)
+        {
+            workbook = excel.Workbooks.Add();
+            worksheet = workbook.ActiveSheet;
+
+            // 엑셀 파일에 데이터 쓰기
+            worksheet.Cells[1, 1] = rtbResponse.Text;
+
+            // 엑셀 파일 저장
+            workbook.SaveAs("C:\\Temp\\test.xlsx");
+            workbook.Close();
+            excel.Quit();
+
+            MessageBox.Show("Data saved to Excel file.");
+        }
+    }
+}
